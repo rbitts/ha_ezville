a732a1784e50b20665392e1036d7a95298c9a492
root <root@PlexLXC.192.168.1.1>
2025-11-29 00:33:52 +0900

    fix: mqttt connection

--- simple_mqtt_ezville_control/ezville.py
+++ simple_mqtt_ezville_control/ezville.py
@@ -262,8 +262,8 @@ def ezville_loop(config):
   
 
     # MQTT 통신 연결 Callback
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
+    def on_connect(client, userdata, flags, reason_code, properties):
+        if reason_code == 0:
             log('[INFO] MQTT Broker 연결 성공')
             # Socket인 경우 MQTT 장치의 명령 관련과 MQTT Status (Birth/Last Will Testament) Topic만 구독
             if comm_mode == 'socket':
@@ -280,7 +280,7 @@ def ezville_loop(config):
                        3: 'Connection refused - server unavailable',
                        4: 'Connection refused - bad username or password',
                        5: 'Connection refused - not authorised'}
-            log(errcode[rc])
+            log(errcode.get(reason_code.value, f'Connection refused - unknown reason: {reason_code}'))
          
         
     # MQTT 메시지 Callback
@@ -309,7 +309,7 @@ def ezville_loop(config):
  
 
     # MQTT 통신 연결 해제 Callback
-    def on_disconnect(client, userdata, rc):
+    def on_disconnect(client, userdata, flags, reason_code, properties):
         log('INFO: MQTT 연결 해제')
         pass
 
@@ -991,7 +991,7 @@ def ezville_loop(config):
 
         
     # MQTT 통신
-    mqtt_client = mqtt.Client('mqtt-ezville')
+    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, 'mqtt-ezville')
     mqtt_client.username_pw_set(config['mqtt_id'], config['mqtt_password'])
     mqtt_client.on_connect = on_connect
     mqtt_client.on_disconnect = on_disconnect
